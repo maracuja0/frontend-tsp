6fffdeb033d90a858b99f4f409604d7646f0cd04
maracuja0 <110384417+maracuja0@users.noreply.github.com>
2024-04-19 21:19:40 +0400

    Добавил бутстрап в проект

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import axios from 'axios'
 
 function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8080/api/user")
+            .then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited by Maracuja0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <Button>Test Button</Button>
       </header>
     </div>
   );
